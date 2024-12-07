574455cbcc1b745af73a39e170838f349a5fa5bf
lucavehbiu <lucavehbiu96@gmail.com>
2024-12-07 16:01:41 +0100

    wow

--- src/components/ClientLandingWrapper.tsx
+++ src/components/ClientLandingWrapper.tsx
@@ -1,15 +1,6 @@
 'use client'
 
-import dynamic from 'next/dynamic'
-import { LoadingSpinner } from '@/components/ui/LoadingSpinner'
-
-const LandingPage = dynamic(
-  () => import('./LandingPage').then(mod => mod.default),
-  {
-    ssr: false,
-    loading: () => <LoadingSpinner variant="full" />
-  }
-)
+import LandingPage from '@/components/LandingPage'
 
 export default function ClientLandingWrapper() {
   return <LandingPage />
